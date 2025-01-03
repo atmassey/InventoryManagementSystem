caf9b6afe431954a055cd0aa56195e46e2a8773a
Austin Massey <54543946+atmassey@users.noreply.github.com>
2025-01-03 13:16:38 -0600

    MainForm ui complete

--- InventoryManagementSystem/MainForm.Designer.cs
+++ InventoryManagementSystem/MainForm.Designer.cs
@@ -38,12 +38,17 @@
             SearchParts = new Button();
             SearchProducts = new Button();
             SearchPartTextBox = new TextBox();
-            textBox1 = new TextBox();
+            SearchProductTextBox = new TextBox();
+            PartList = new ListBox();
+            ProductList = new ListBox();
+            PartsLabel = new Label();
+            ProductsLabel = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // Exit
             // 
-            Exit.Location = new Point(1060, 499);
+            Exit.Location = new Point(979, 502);
             Exit.Name = "Exit";
             Exit.Size = new Size(73, 34);
             Exit.TabIndex = 0;
@@ -53,7 +58,7 @@
             // 
             // AddPart
             // 
-            AddPart.Location = new Point(202, 319);
+            AddPart.Location = new Point(252, 335);
             AddPart.Name = "AddPart";
             AddPart.Size = new Size(80, 30);
             AddPart.TabIndex = 1;
@@ -62,7 +67,7 @@
             // 
             // ModifyPart
             // 
-            ModifyPart.Location = new Point(288, 319);
+            ModifyPart.Location = new Point(338, 335);
             ModifyPart.Name = "ModifyPart";
             ModifyPart.Size = new Size(80, 30);
             ModifyPart.TabIndex = 2;
@@ -71,7 +76,7 @@
             // 
             // DeletePart
             // 
-            DeletePart.Location = new Point(374, 319);
+            DeletePart.Location = new Point(424, 335);
             DeletePart.Name = "DeletePart";
             DeletePart.Size = new Size(80, 30);
             DeletePart.TabIndex = 3;
@@ -80,7 +85,7 @@
             // 
             // AddProduct
             // 
-            AddProduct.Location = new Point(842, 325);
+            AddProduct.Location = new Point(800, 335);
             AddProduct.Name = "AddProduct";
             AddProduct.Size = new Size(80, 30);
             AddProduct.TabIndex = 4;
@@ -89,7 +94,7 @@
             // 
             // ModifyProduct
             // 
-            ModifyProduct.Location = new Point(928, 325);
+            ModifyProduct.Location = new Point(886, 335);
             ModifyProduct.Name = "ModifyProduct";
             ModifyProduct.Size = new Size(80, 30);
             ModifyProduct.TabIndex = 5;
@@ -98,7 +103,7 @@
             // 
             // DeleteProduct
             // 
-            DeleteProduct.Location = new Point(1014, 325);
+            DeleteProduct.Location = new Point(972, 335);
             DeleteProduct.Name = "DeleteProduct";
             DeleteProduct.Size = new Size(80, 30);
             DeleteProduct.TabIndex = 6;
@@ -107,7 +112,7 @@
             // 
             // SearchParts
             // 
-            SearchParts.Location = new Point(405, 58);
+            SearchParts.Location = new Point(258, 74);
             SearchParts.Name = "SearchParts";
             SearchParts.Size = new Size(60, 25);
             SearchParts.TabIndex = 7;
@@ -116,7 +121,7 @@
             // 
             // SearchProducts
             // 
-            SearchProducts.Location = new Point(862, 60);
+            SearchProducts.Location = new Point(806, 76);
             SearchProducts.Name = "SearchProducts";
             SearchProducts.Size = new Size(60, 25);
             SearchProducts.TabIndex = 8;
@@ -125,24 +130,79 @@
             // 
             // SearchPartTextBox
             // 
-            SearchPartTextBox.Location = new Point(471, 60);
+            SearchPartTextBox.Location = new Point(324, 76);
             SearchPartTextBox.Name = "SearchPartTextBox";
             SearchPartTextBox.Size = new Size(180, 23);
             SearchPartTextBox.TabIndex = 9;
             // 
-            // textBox1
+            // SearchProductTextBox
             // 
-            textBox1.Location = new Point(928, 60);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(180, 23);
-            textBox1.TabIndex = 10;
+            SearchProductTextBox.Location = new Point(872, 76);
+            SearchProductTextBox.Name = "SearchProductTextBox";
+            SearchProductTextBox.Size = new Size(180, 23);
+            SearchProductTextBox.TabIndex = 10;
+            // 
+            // PartList
+            // 
+            PartList.FormattingEnabled = true;
+            PartList.ItemHeight = 15;
+            PartList.Location = new Point(24, 118);
+            PartList.Name = "PartList";
+            PartList.Size = new Size(480, 199);
+            PartList.TabIndex = 11;
+            // 
+            // ProductList
+            // 
+            ProductList.FormattingEnabled = true;
+            ProductList.ItemHeight = 15;
+            ProductList.Location = new Point(572, 118);
+            ProductList.Name = "ProductList";
+            ProductList.Size = new Size(480, 199);
+            ProductList.TabIndex = 12;
+            // 
+            // PartsLabel
+            // 
+            PartsLabel.AutoSize = true;
+            PartsLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PartsLabel.Location = new Point(24, 94);
+            PartsLabel.Name = "PartsLabel";
+            PartsLabel.Size = new Size(44, 21);
+            PartsLabel.TabIndex = 13;
+            PartsLabel.Text = "Parts";
+            PartsLabel.Click += label1_Click;
+            // 
+            // ProductsLabel
+            // 
+            ProductsLabel.AutoSize = true;
+            ProductsLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ProductsLabel.Location = new Point(572, 94);
+            ProductsLabel.Name = "ProductsLabel";
+            ProductsLabel.Size = new Size(71, 21);
+            ProductsLabel.TabIndex = 14;
+            ProductsLabel.Text = "Products";
+            ProductsLabel.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(227, 21);
+            label3.TabIndex = 15;
+            label3.Text = "Inventory Management System";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1145, 545);
-            Controls.Add(textBox1);
+            ClientSize = new Size(1078, 550);
+            Controls.Add(label3);
+            Controls.Add(ProductsLabel);
+            Controls.Add(PartsLabel);
+            Controls.Add(ProductList);
+            Controls.Add(PartList);
+            Controls.Add(SearchProductTextBox);
             Controls.Add(SearchPartTextBox);
             Controls.Add(SearchProducts);
             Controls.Add(SearchParts);
@@ -172,6 +232,11 @@
         private Button SearchParts;
         private Button SearchProducts;
         private TextBox SearchPartTextBox;
-        private TextBox textBox1;
+        private TextBox SearchProductTextBox;
+        private ListBox PartList;
+        private ListBox ProductList;
+        private Label PartsLabel;
+        private Label ProductsLabel;
+        private Label label3;
     }
 }
