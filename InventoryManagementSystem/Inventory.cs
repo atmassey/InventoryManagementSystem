13eb131d2769811fdae4c29688a83f72600e10eb
Austin Massey <54543946+atmassey@users.noreply.github.com>
2025-01-04 07:08:52 -0600

    added classes for app structure

---
+++ InventoryManagementSystem/Inventory.cs
@@ -0,0 +1,7 @@
+﻿namespace InventoryManagementSystem
+{
+    public class Inventory
+    {
+
+    }
+}
---
+++ InventoryManagementSystem/Part.cs
@@ -0,0 +1,7 @@
+﻿namespace InventoryManagementSystem
+{
+    public abstract class Part
+    {
+
+    }
+}
